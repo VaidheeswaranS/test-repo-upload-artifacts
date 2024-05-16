9aea017e9cbdaa34e4fbf21780df842a32aa9c12
VaidheeswaranS <76675886+VaidheeswaranS@users.noreply.github.com>
2024-05-16 19:17:33 +0530

    Update script.ps1

--- sample/script.ps1
+++ sample/script.ps1
@@ -1,3 +1,16 @@
+# Create the necessady folder if not present
+$folderPath = "C:\dev\Key"
+
+# Check if the folder exists
+if (-not (Test-Path -Path $folderPath)) {
+    # Create the folder
+    New-Item -Path $folderPath -ItemType Directory
+    Write-Output "Folder 'Key' created at C:\dev"
+} else {
+    Write-Output "Folder 'Key' already exists at C:\dev"
+}
+
+# Make changes to the file mentioning the sage 200 private key path
 $filePath = "C:\Program Files (x86)\MSBuild\14.0\Microsoft.Common.Targets\ImportAfter\Sage200.Signing.targets"
 $oldContent = "<KeyFilePath>[ReplaceFileShareLocation]\Key\Sage200v2013PrivateKey.snk</KeyFilePath>"
 $newContent = "<KeyFilePath>C:\Dev\Key\Sage200v2013PrivateKey.snk</KeyFilePath"
